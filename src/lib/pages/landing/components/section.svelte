379a43792f45106b29458c57c764bc07682e81a2
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-19 15:59:46 -0500

    Remove direction support & improve sizing.

--- src/lib/pages/landing/components/section.svelte
+++ src/lib/pages/landing/components/section.svelte
@@ -2,7 +2,6 @@
     import { onMount } from "svelte";
 
     export let id: string;
-    export let direction: "left" | "right" = "left";
     export let title: string;
     export let description: string;
     export let color: string = "#ffffff";
@@ -17,8 +16,8 @@
 
 <span {id} class="relative" />
 
-<div class="section flex flex-col justify-center md:justify-between items-center w-full h-screen px-10 relative {direction === 'right' ? 'md:flex-row-reverse' : 'md:flex-row'}" style="--sectionColor: {color}; --sectionColorTransparent: {color}80;" id="#{id}" bind:this={sectionContainer}>
-    <div class="w-full md:w-[45%] h-full text-center md:text-left flex flex-col justify-center items-center md:items-start gap-5 md:gap-7 {direction === 'right' ? 'md:pr-[8%] 2xl:pr-[12%]' : 'md:pl-[8%] 2xl:pl-[12%]'}">
+<div class="section flex flex-col justify-center md:justify-between items-center w-full h-screen px-10 relative md:flex-row" style="--sectionColor: {color}; --sectionColorTransparent: {color}80;" id="#{id}" bind:this={sectionContainer}>
+    <div class="w-full md:w-[47%] h-full text-center md:text-left flex flex-col justify-center items-center md:items-start gap-5 md:gap-7 md:pl-[8%] 2xl:pl-[12%]">
         <p class="text-4xl font-semibold md:font-bold tracking-tight">{@html title}</p>
         <p class="text-lg">{description}</p>
 
