921c2938592c223053de75a596c5cc79ebd749a3
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-19 20:31:25 -0500

    Add support for two rotation states.

--- src/lib/pages/landing/components/hero/thought.svelte
+++ src/lib/pages/landing/components/hero/thought.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
     export let style: string;
     export let delay: number = 0;
-    export let rotation: number = 0;
+    export let rotation1: number = 0;
+    export let rotation2: number = 0;
 </script>
 
-<div class="float matchHeroColor absolute h-10 rounded-full bg-[var(--wordColor)] flex justify-center items-center" style={style.concat(`--delay: ${delay}s;`, `--rotation: ${rotation}deg;`)}>
+<div class="float matchHeroColor absolute h-10 rounded-full bg-[var(--wordColor)] flex justify-center items-center" style={style.concat(`--delay: ${delay}s; --rotation1: ${rotation1}deg; --rotation2: ${rotation2}deg;`)}>
     <slot />
 </div>
