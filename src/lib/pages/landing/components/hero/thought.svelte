4d3856f9fcbd1631299e7427d6018443b1a8f05e
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-19 21:10:15 -0500

    Add animation.

--- src/lib/pages/landing/components/hero/thought.svelte
+++ src/lib/pages/landing/components/hero/thought.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
+    import { onMount } from "svelte";
+    import { animate } from "motion";
+    import { easeInOutCubic } from "$lib/easings";
+
     export let style: string;
     export let delay: number = 0;
     export let rotation1: number = 0;
     export let rotation2: number = 0;
+
+    let thoughtContainer: HTMLDivElement;
+
+    onMount(() => {
+        let thoughtContainerWidth: number = thoughtContainer.clientWidth;
+
+        animate(
+            thoughtContainer,
+            {
+                opacity: [0, 1],
+                width: [`${thoughtContainerWidth / 3}px`, `${thoughtContainerWidth}px`],
+            },
+            {
+                easing: easeInOutCubic,
+                duration: 1,
+                delay: delay,
+            }
+        );
+    });
 </script>
 
-<div class="float matchHeroColor absolute h-10 rounded-full bg-[var(--wordColor)] flex justify-center items-center" style={style.concat(`--delay: ${delay}s; --rotation1: ${rotation1}deg; --rotation2: ${rotation2}deg;`)}>
+<div class="float matchHeroColor absolute h-10 rounded-full bg-[var(--wordColor)] flex justify-center items-center" style={style.concat(`--delay: ${delay}s; --rotation1: ${rotation1}deg; --rotation2: ${rotation2}deg;`)} bind:this={thoughtContainer}>
     <slot />
 </div>
