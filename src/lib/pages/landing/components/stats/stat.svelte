7a85399f8c30b8c531c855cd33c1ca580242df5b
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-22 07:40:21 -0500

    Add stats.

---
+++ src/lib/pages/landing/components/stats/stat.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    export let number: string = "0%";
+    export let name: string = "";
+    export let color: string = "#ffffff";
+</script>
+
+<div class="relative h-[60vh] w-full md:h-full md:w-1/3" style="--sectionColor: {color}; --sectionShadowColor: {color}80;">
+    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+        <p class="highlight scrollAnimation fromLeft text-6xl font-bold tracking-tight">{number}</p>
+        <p class="highlight scrollAnimation fromLeft mt-1 text-xl font-medium tracking-tight">{name}</p>
+    </div>
+</div>
--- src/lib/pages/landing/stats.svelte
+++ src/lib/pages/landing/stats.svelte
@@ -1,1 +1,9 @@
-<div class="relative h-fit min-h-screen md:h-screen"></div>
+<script lang="ts">
+    import Stat from "./components/stats/stat.svelte";
+</script>
+
+<div class="section relative flex h-fit min-h-screen flex-col px-0 md:h-screen md:flex-row md:px-10">
+    <Stat number="22%" name="increase user feedback" color="#66FF99" />
+    <Stat number="30%" name="faster feedback resolution" color="#FF6666" />
+    <Stat number="95%" name="user satisfaction rating" color="#FFD700" />
+</div>
