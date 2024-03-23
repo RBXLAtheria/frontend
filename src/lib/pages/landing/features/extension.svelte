788e3cbc11234710a621fda410c6584b50a65019
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-23 11:28:58 -0500

    Add extension section.

---
+++ src/lib/pages/landing/features/extension.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Section from "../components/section.svelte";
+</script>
+
+<Section id="features-extension" color="#FF5733" title={`Stay studio-bound,<br><span class="highlight">with the extension.</span>`} description="Stay Studio-Bound with Atheria's extension, offering seamless access to manage all feedback directly within Studio. This integrated tool ensures you never lose sight of valuable insights while you work on perfecting your creations. With Atheria's extension, every comment and suggestion is easily accessible, empowering you to refine and enhance your game without interruption." tags={["features"]}></Section>
