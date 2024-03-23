b438564eac528a03e872c4e8b812b0be34c15047
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-03-22 21:24:19 -0500

    Improve stats section.

--- src/lib/pages/landing/components/stats/stat.svelte
+++ src/lib/pages/landing/components/stats/stat.svelte
@@ -11,7 +11,7 @@
         <p class="highlight scrollAnimation fromLeft mt-1 text-xl font-medium tracking-tight">{name}</p>
     </div>
 
-    <div class="absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 md:h-3/4">
+    <div class="scrollAnimation fromOpacity absolute left-1/2 top-1/2 h-full w-full -translate-x-1/2 -translate-y-1/2 md:h-3/4">
         <slot />
     </div>
 </div>
--- src/lib/pages/landing/stats.svelte
+++ src/lib/pages/landing/stats.svelte
@@ -2,7 +2,7 @@
     import Stat from "./components/stats/stat.svelte";
 </script>
 
-<div class="section relative flex h-fit min-h-screen flex-col gap-2 px-0 md:h-screen md:flex-row md:px-10" id="section-stats">
+<div class="section relative mb-10 flex h-fit min-h-screen flex-col gap-2 px-0 md:h-screen md:flex-row md:px-10" id="section-stats">
     <Stat id="feedbackIncrease" number="22%" name="increase in user feedback" color="#66FF99">
         {#each Array(10) as _value, index}
             <svg class="arrow absolute aspect-square -translate-y-1/2 fill-[--sectionColor]" style="width: {Math.random() * 2 + 2}rem; {Math.random() > 0.5 ? 'left' : 'right'}: {Math.random() * 50}%; --speed: {Math.random() * 2.5 + 1}s; --delay: {index / 2}s; --startY: {100 - Math.random() * 5}%;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -25,7 +25,7 @@
 
     <Stat id="ratingIncrease" number="95%" name="average increase in rating" color="#FFD700">
         {#each Array(4) as _value, quadrant}
-            <div class="absolute h-[calc(50%-5%)] w-[calc(50%-5%)]" style="top: calc({25 * (quadrant == 0 || quadrant == 1 ? 0 : 2)}% + {5 * (quadrant == 0 || quadrant == 1 ? 0 : 1)}%); left: calc({25 * (quadrant == 0 || quadrant == 2 ? 0 : 2)}% + {5 * (quadrant == 0 || quadrant == 2 ? 0 : 1)}%);">
+            <div class="absolute h-[calc(50%-8%)] w-[calc(50%-8%)]" style="top: calc({25 * (quadrant == 0 || quadrant == 1 ? 0 : 2)}% + {8 * (quadrant == 0 || quadrant == 1 ? 0 : 1)}%); left: calc({25 * (quadrant == 0 || quadrant == 2 ? 0 : 2)}% + {8 * (quadrant == 0 || quadrant == 2 ? 0 : 1)}%);">
                 {#each Array(5) as _value, index}
                     <svg class="heart absolute aspect-square fill-[--sectionColor]" style="width: {Math.random() * 2 + 1.5}rem; {quadrant == 0 || quadrant == 2 ? 'left' : 'right'}: {Math.random() * 100}%; top: {Math.random() * 100}%; --delay: {index + (quadrant + 1)}s;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
